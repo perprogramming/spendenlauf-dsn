1ff5f925a0427dae6c800509f489aaee382ebe2d
Per Bernhardt <per.bernhardt@leanix.net>
2020-04-20 17:44:22 +0200

    Setup ORM

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "" > .env.local
-echo "APP_ENV=prod" >> .env.local
+echo "APP_ENV=dev" >> .env.local
+echo "DATABASE_URL=mysql://db49905_13:$DB_PASSWORD@mysql5.sinfin.de/db49905_13?serverVersion=5.6.19" >> .env.local
 
 composer install
 
