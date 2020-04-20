7b46698aaec5e3f91fae8728577b72ab5e7b3701
Per Bernhardt <per.bernhardt@leanix.net>
2020-04-20 18:10:44 +0200

    Fix migrations

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ echo "DATABASE_URL=mysql://db49905_13:$DB_PASSWORD@mysql5.sinfin.de/db49905_13?s
 
 composer install
 
-rsync --progress -r --links --exclude-from=.rsyncignore --delete . ssh-49905-plb@sinfin.de:/kunden/sinfin.de/webseiten/spendenlauf.perbernhardt.de
+rsync -r --links --exclude-from=.rsyncignore --delete . ssh-49905-plb@sinfin.de:/kunden/sinfin.de/webseiten/spendenlauf.perbernhardt.de
 
-ssh ssh-49905-plb@sinfin.de "cd /kunden/sinfin.de/webseiten/spendenlauf.perbernhardt.de && /usr/local/bin/php73 bin/console doctrine:migrations:migrate"
+ssh ssh-49905-plb@sinfin.de "cd /kunden/sinfin.de/webseiten/spendenlauf.perbernhardt.de && /usr/local/bin/php73 bin/console doctrine:migrations:migrate -n --allow-no-migration"
 
 rm .env.local || true
